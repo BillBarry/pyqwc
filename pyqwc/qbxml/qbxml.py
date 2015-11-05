66b80533ad1e6dbc66a88e61150be627980971a5
Bill Barry <bill@billbarry.org>
2015-11-05 01:59:02 -0600

    start expanding qbxml we can handle

--- pyqwc/qbxml/qbxml.py
+++ pyqwc/qbxml/qbxml.py
@@ -21,3 +21,30 @@ def iterative_query_request(requestID=1,iteratorID="",querytype="",MaxReturned=1
     requestxml = etree.tostring(tree, xml_declaration=True, encoding='UTF-8')
     return requestxml
 
+def invoice_query(requestID=1,iteratorID="",querytype="",fullname="",IncludeLineItems=False,MaxReturned=100):
+    attributes = {}
+    if not iteratorID:
+        attributes['iterator'] = "Start"
+    else:
+        attributes['iterator'] = "Continue"
+        attributes['iteratorID'] = iteratorID        
+    attributes['requestID'] = str(requestID)
+    root = etree.Element("QBXML")
+    root.addprevious(etree.ProcessingInstruction("qbxml", "version=\"8.0\""))
+    msg = etree.SubElement(root,'QBXMLMsgsRq', {'onError':'stopOnError'})
+    irq = etree.SubElement(msg,querytype+'QueryRq',attributes)
+    mrt = etree.SubElement(irq,'MaxReturned')
+    mrt.text= str(MaxReturned)
+
+    mrt = etree.SubElement(irq,'EntityFilter')
+    ef = etree.SubElement(mrt,'FullNameWithChildren')
+    ef.text= str(fullname)
+    if querytype == 'Invoice' and IncludeLineItems:
+        incitems = etree.SubElement(irq,'IncludeLineItems')
+        incitems.text="true"
+    includelinkedtxns = etree.SubElement(irq,'IncludeLinkedTxns')
+    includelinkedtxns.text="true"
+    tree = etree.ElementTree(root)
+    requestxml = etree.tostring(tree, xml_declaration=True, encoding='UTF-8')
+    return requestxml
+
